3ea4fc992746c173692f8bd944a14dbbb82cba99
Ankita Survase <ankitasurvase134@gmail.com>
2026-07-15 16:05:51 +0530

    Time: 1154 ms (96.61%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+#first write the table on the left then leftjoin the table on the right the from table should be the one whose all data is needed
 SELECT PRODUCT_NAME,YEAR,PRICE
 FROM SALES S
 LEFT JOIN PRODUCT P
